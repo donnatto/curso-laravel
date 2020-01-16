f25f68d1f3574c706fda60d5290d306cf7bc348b
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-15 21:20:25 -0500

    add views and controllers for edit and delete

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -18,6 +18,8 @@
                 @foreach($expenseReports as $expenseReport)
                 <tr>
                     <td>{{ $expenseReport->title }}</td>
+                    <td><a href="/expense_reports/{{ $expenseReport->id }}/edit">Edit</a></td>
+                    <td><a href="/expense_reports/{{ $expenseReport->id }}/confirmDelete">Delete</a></td>
                 </tr>
                 @endforeach
             </table>
